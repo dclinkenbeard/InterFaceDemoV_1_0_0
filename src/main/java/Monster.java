cd63c2acd41e53e3fc26f7e49991fa45b1b60389
Dr. Drew Clinkenbeard <dclinkenbeard@csumb.edu>
2025-02-12 09:27:25 -0800

    ✨ Added Movement interface and WalkingMovment that implements it.

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -8,7 +8,7 @@ public class Monster {
   Double strength;
   Double defense;
 
-
+  Movement movement = new WalkingMovement();
 
   @Override
   public final boolean equals(Object o) {
---
+++ src/main/java/WalkingMovement.java
@@ -0,0 +1,7 @@
+public class WalkingMovement implements Movement{
+
+  @Override
+  public void movementMethod() {
+    System.out.println("Can't you see I'm WALKIN' here?!");
+  }
+}
